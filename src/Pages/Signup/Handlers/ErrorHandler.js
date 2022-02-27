c216234f6784ef8243c656894136fbb91cdf04f9
Navid079 <navid.lee79@gmail.com>
2022-02-27 08:07:16 +0330

    signup page pre call validator added

--- src/Pages/Signup/Handlers/ErrorHandler.js
+++ src/Pages/Signup/Handlers/ErrorHandler.js
@@ -1,3 +1,5 @@
+import validator from 'validator';
+
 import errorFinder from '../../../Logic/Errors/ErrorFinder';
 
 export const errorHandler = (states, errorCode) => {
@@ -8,4 +10,31 @@ export const errorHandler = (states, errorCode) => {
   } catch {
     console.log('Not an error. Maybe something is wrong?');
   }
+};
+
+export const preCallValidator = ({ fullName, username, password, phone }) => {
+  if (!fullName) return 0x1000;
+  if (!username) return 0x1001;
+  if (!password) return 0x1002;
+  if (!phone) return 0x1003;
+
+  if (
+    !validator.isLength(username, { min: 4, max: 25 }) ||
+    !validator.matches(username, /^[a-zA-Z]+[0-9a-zA-Z_]*$/)
+  )
+    return 0x1004;
+
+  if (!validator.matches(phone, /^(0|0098|098|\+98|98)?9[0-9]{9}$/))
+    return 0x1005;
+  if (
+    !validator.isStrongPassword(password, {
+      minLength: 8,
+      minLowercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+      minUppercase: 1,
+    })
+  )
+    return 0x1006;
+  return 0;
 };
--- src/Pages/Signup/Handlers/EventHandlers.js
+++ src/Pages/Signup/Handlers/EventHandlers.js
@@ -1,14 +1,18 @@
 import SignupCall from '../../../Logic/API/LoginSignup/SignupCall';
-import { errorHandler } from './ErrorHandler';
+import { errorHandler, preCallValidator } from './ErrorHandler';
 
 export const submitHandler = async (refs, states, navigate) => {
   const { fullNameInput, usernameInput, passwordInput, phoneInput } = refs;
-  const result = await SignupCall({
-    fullName: fullNameInput.current.value,
-    username: usernameInput.current.value,
-    password: passwordInput.current.value,
-    phone: phoneInput.current.value,
-  });
+  const fullName = fullNameInput.current.value;
+  const username = usernameInput.current.value;
+  const password = passwordInput.current.value;
+  const phone = phoneInput.current.value;
+
+  let result = preCallValidator({ fullName, username, password, phone });
+  if (result === 0) {
+    console.log('API Call occured');
+    result = await SignupCall({ fullName, username, password, phone });
+  }
   if (result === 0) navigate('/');
   else {
     const errorCode = result.toString(16);
