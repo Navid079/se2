498a72b99cd12472134ac6657193b6a6c902c06a
Navid079 <navid.lee79@gmail.com>
2022-02-27 05:42:22 +0330

    added color to input focus border

--- src/Components/UI/TextInput/IconInput.js
+++ src/Components/UI/TextInput/IconInput.js
@@ -1,18 +1,30 @@
 // Libraries
-import React from 'react';
+import React, { useRef } from 'react';
 
 // Stylesheets
 import './IconInput.css';
 
 const IconInput = ({ className, placeholder, type, onChange, icon }) => {
+  const containerRef = useRef();
+
+  const focusHandler = event => {
+    containerRef.current.style.borderColor = 'var(--clr-tertiary-5)';
+  };
+
+  const blurHandler = event => {
+    containerRef.current.style.borderColor = '';
+  };
+
   return (
-    <span className={`text-input__container ${className}`}>
+    <span className={`text-input__container ${className}`} ref={containerRef}>
       {icon}
       <input
         className="text-input"
         placeholder={placeholder}
         type={type || 'text'}
         onChange={onChange}
+        onFocus={focusHandler}
+        onBlur={blurHandler}
       />
     </span>
   );
