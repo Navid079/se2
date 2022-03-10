06ea80ae1fdef36d143062eb2f4b9b8734fa3b74
Haniye Jahanbakhsh <haniahn1115@gmail.com>
2022-03-10 23:05:48 +0330

    profile picture component aded

---
+++ src/Components/UI/ProfPic/ProfPic.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './ProfPic.css';
+
+import { MdPerson } from 'react-icons/md';
+
+export default function ProfPic() {
+  return (
+    <div className="profpic">
+      <MdPerson className="profpic__icon" />
+    </div>
+  );
+}
