744dce7be3dc599496985b7e896895b90441c023
Haniye Jahanbakhsh <haniahn1115@gmail.com>
2022-03-11 18:23:34 +0330

    footer component and styles added

---
+++ src/Components/UI/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Footer.css';
+import { MdPerson } from 'react-icons/md';
+import { FaHome } from 'react-icons/fa';
+import { MdSettings } from 'react-icons/md';
+
+export default function Footer({ className }) {
+  return (
+    <footer className={`footer ${className}`}>
+      <div className="footer-item">
+        <MdPerson className="footer-item__icon" />
+        <p className="footer-item__title">پروفایل</p>
+      </div>
+      <div className="footer-item">
+        <FaHome className="footer-item__icon" />
+        <p className="footer-item__title">خانه</p>
+      </div>
+      <div className="footer-item">
+        <MdSettings className="footer-item__icon" />
+        <p className="footer-item__title">تنظیمات</p>
+      </div>
+    </footer>
+  );
+}
