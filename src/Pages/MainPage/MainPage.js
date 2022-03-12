d3b96e144a41adfbdc2b7650b77a91965efbc2f0
Haniye Jahanbakhsh <haniahn1115@gmail.com>
2022-03-12 13:12:40 +0330

    removed extra lines :)

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -10,28 +10,29 @@ import './MainPage.css';
 
 export default function MainPage() {
   return (
-  
-  <div className="main__page">
-      <div className='top__menu'>
-      <p className='title'>روزت رو با کتاب شروع کن یا تموم کن</p>
-      <div className='search'><HiSearch/></div>
+    <div className="main__page">
+      <div className="top__menu">
+        <p className="title">روزت رو با کتاب شروع کن یا تموم کن</p>
+        <div className="search">
+          <HiSearch />
+        </div>
       </div>
       <nav className="bottom__navbar">
         <div className="navbar__items">
-           <div className='item setting'> <IoMdSettings/> <p>Setting</p> </div> 
-           <div className='item plus'> <BsFillPlusCircleFill/> <p>Home</p> </div>
-           <div className='item profile'> <MdPerson/> <p>Profile</p> </div> 
+          <div className="item setting">
+            <IoMdSettings />
+            <p>Setting</p>
+          </div>
+          <div className="item plus">
+            <BsFillPlusCircleFill />
+            <p>Home</p>
+          </div>
+          <div className="item profile">
+            <MdPerson />
+            <p>Profile</p>
+          </div>
         </div>
       </nav>
-
-
-      
-      
-      
-      </div>
-
-
-
-
+    </div>
   );
 }
