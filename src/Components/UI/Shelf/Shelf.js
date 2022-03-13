247db6a17fc59960e9d7f8c4acc040588a6584c9
Haniye Jahanbakhsh <haniahn1115@gmail.com>
2022-03-14 01:37:55 +0330

    shelf styles added

--- src/Components/UI/Shelf/Shelf.js
+++ src/Components/UI/Shelf/Shelf.js
@@ -11,15 +11,17 @@ export default function Shelf({ className, title }) {
   return (
     <div className={`shelf ${className}`}>
       <h1 className="shelf__header">{title}</h1>
-      <hr className="shelf__header-line" />
-      <div className="shelf__books">
-        <Book className="shelf__book" />
-        <Book className="shelf__book" />
-        <Book className="shelf__book" />
-        <Book className="shelf__book" />
-        <Book className="shelf__book" />
-        <Book className="shelf__book" />
-        <Book className="shelf__book" />
+      <div className="shelf__container">
+        <hr className="shelf__header-line" />
+        <div className="shelf__books">
+          <Book className="shelf__book" />
+          <Book className="shelf__book" />
+          <Book className="shelf__book" />
+          <Book className="shelf__book" />
+          <Book className="shelf__book" />
+          <Book className="shelf__book" />
+          <Book className="shelf__book" />
+        </div>
       </div>
     </div>
   );
