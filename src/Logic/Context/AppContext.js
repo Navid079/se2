a950ce74f8363bcf9a9d2a7d4e0ca31dc9b2269c
Haniye Jahanbakhsh <haniahn1115@gmail.com>
2022-03-13 12:47:05 +0330

    AppContext added

---
+++ src/Logic/Context/AppContext.js
@@ -0,0 +1,11 @@
+const { createContext } = require('react');
+
+const INITIAL = {
+  currentPage: 'landing',
+};
+
+const AppContext = createContext(INITIAL);
+
+const AppContextProvider = ({ children }) => {
+  return <AppContext.Provider>{children}</AppContext.Provider>;
+};
