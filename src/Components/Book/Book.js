25e3aa2920eba2b1117ab1eddac26189feafc4f0
Haniye Jahanbakhsh <haniahn1115@gmail.com>
2022-03-13 23:07:44 +0330

    Book stars added

--- src/Components/Book/Book.js
+++ src/Components/Book/Book.js
@@ -2,16 +2,36 @@ import React from 'react';
 
 import './Book.css';
 import img from '../../images/navbar-colorful-books.png';
+import { FaStar } from 'react-icons/fa';
 
 export default function Book({ className }) {
   const bookTitle = 'عنوان کتاب';
   const bookImg = img;
   const bookPrice = '20000';
+  const stars = 5;
+  const starComponents = [];
+  for (let i = 0; i < stars; i++) {
+    starComponents.push(<FaStar className="book__star" />);
+  }
+
+  const starClass = stars => {
+    if (stars <= 3) {
+      return 'book__star-3';
+    } else if (stars === 4) {
+      return 'book__star-4';
+    } else if (stars === 5) {
+      return 'book__star-5';
+    }
+  };
+
   return (
     <div className={`book ${className}`}>
       <img className="book__img" src={bookImg} />
       <h3 className="book__title">{bookTitle}</h3>
       <h4 className="book__price">{bookPrice}</h4>
+      <div className={`book__star-container ${starClass(stars)}`}>
+        {starComponents}
+      </div>
     </div>
   );
 }
