64375afa63b10009a5ca5dc3d7e2ba9222cc004c
Haniye Jahanbakhsh <haniahn1115@gmail.com>
2022-03-14 21:11:25 +0330

    login validation added

--- src/Pages/HomeWrapper/HomeWrapper.js
+++ src/Pages/HomeWrapper/HomeWrapper.js
@@ -1,17 +1,24 @@
 // Libraries
-import React, { useContext } from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 // Components
 import Footer from '../../Components/UI/Footer/Footer';
 import Navbar from '../../Components/UI/Navbar/Navbar';
 import AppContext from '../../Logic/Context/AppContext/AppContext';
+import UserContext from '../../Logic/Context/UserContext/UserContext';
 
 // Stylesheets
 import './HomeWrapper.css';
 
 export default function HomeWrapper() {
   const { currentPage } = useContext(AppContext);
+  const { isLoggedIn } = useContext(UserContext);
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (!isLoggedIn) navigate('/');
+  });
 
   return (
     <>
