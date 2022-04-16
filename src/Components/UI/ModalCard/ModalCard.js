d92062a60d58f599c7998ec05d5fbdcc5dab3e1a
Haniye Jahanbakhsh <haniahn1115@gmail.com>
2022-04-16 23:06:28 +0430

    ModalCard component added

---
+++ src/Components/UI/ModalCard/ModalCard.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import './ModalCard.css';
+
+export default function ModalCard({ show, onClose, children }) {
+  const closeOnEscapeKeyDown = e => {
+    if ((e.charCode || e.keyCode) === 27) {
+      onClose();
+    }
+  };
+  
+  useEffect(() => {
+    document.body.addEventListener('keydown', closeOnEscapeKeyDown);
+    return function cleanup() {
+      document.body.removeEventListener('keydown', closeOnEscapeKeyDown);
+    };
+  }, []);
+
+  if (!show) return null;
+
+  return (
+    <div className="modal-card" onClick={onClose}>
+      <div className="modal__content" onClick={e => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>
+  );
+}
