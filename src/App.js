72c0b3b6ab6d964a9957efccd6284485176937a4
SaraM2000 <saramovahhed@yahoo.com>
2022-04-18 12:54:55 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -3,21 +3,19 @@ import React from 'react';
 
 // Components
 import Context from './Logic/App/Context';
-import Router from "./Logic/App/Router";
+import Router from './Logic/App/Router';
 
 // Stylesheets
 import './App.css';
 import './Assets/Stylesheets/Fonts.css';
 import './Assets/Stylesheets/Globals.css';
-import EditProfile from './Components/Profile/EditProfile/EditProfile';
 
 function App() {
   return (
     <div className="App">
-      <EditProfile/>
-      {/* <Context>
+      <Context>
         <Router />
-      </Context> */}
+      </Context>
     </div>
   );
 }
