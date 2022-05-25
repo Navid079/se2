d73f0f86b0f2bfa8eeee71743b982710a08d0f1b
Navid079 <navid.lee79@gmail.com>
2022-05-25 14:23:33 +0430

    added search result link

--- src/Components/SearchItem/SearchItem.js
+++ src/Components/SearchItem/SearchItem.js
@@ -1,5 +1,6 @@
 // Libraries
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 //Components
 import Stars from '../UI/Stars/Stars.js';
@@ -7,21 +8,19 @@ import Stars from '../UI/Stars/Stars.js';
 // Stylesheets
 import './SearchItem.css';
 
-export default function SearchItems({ bookId, author, cover, name, stars}) {
+export default function SearchItems({ bookId, author, cover, name, stars }) {
   return (
-    <div className="search-items">
-      <div className="search-item__details">
-        <img
-          className="search-items__image"
-          src={cover}
-        />
-        <div className="search-items__text">
-          <h1 className="search-items__bookname">{name}</h1>
-          <h3 className="search-items__authorname">{author}</h3>
+    <Link className="g-reset" to={`/app/viewbook/${bookId}`}>
+      <div className="search-items">
+        <div className="search-item__details">
+          <img className="search-items__image" src={cover} />
+          <div className="search-items__text">
+            <h1 className="search-items__bookname">{name}</h1>
+            <h3 className="search-items__authorname">{author}</h3>
+          </div>
         </div>
+        <Stars className="search-item__stars" stars={stars} />
       </div>
-
-      <Stars className="search-item__stars" stars={stars} />
-    </div>
+    </Link>
   );
 }
