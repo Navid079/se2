88ac51dbcd8a5eb3a4b7f416337cd7e47223df11
Navid079 <navid.lee79@gmail.com>
2022-05-26 16:35:03 +0430

    Enter functionality added

--- src/Pages/TextEditor/TextEditor.js
+++ src/Pages/TextEditor/TextEditor.js
@@ -28,12 +28,19 @@ export default function TextEditor() {
               caretLocation - 1
             ].value.slice(0, -1);
           }
+          break;
+        case 'Enter':
+          jsonText.splice(caretLocation, 0, { type: 'newline' });
+          break;
         default:
           break;
       }
     } else {
-      if (caretLocation === 0) {
-        jsonText.splice(0, 0, { type: 'simple', value: key });
+      if (
+        caretLocation === 0 ||
+        jsonText[caretLocation - 1].type === 'newline'
+      ) {
+        jsonText.splice(caretLocation, 0, { type: 'simple', value: key });
       } else {
         jsonText[caretLocation - 1].value += key;
       }
