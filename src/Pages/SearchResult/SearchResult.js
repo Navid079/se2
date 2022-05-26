428428d54808bd1d0a2db3abad4a15705f3ee8b4
SaraM2000 <saramovahhed@yahoo.com>
2022-05-26 16:55:53 +0430

    commite sara

--- src/Pages/SearchResult/SearchResult.js
+++ src/Pages/SearchResult/SearchResult.js
@@ -1,18 +1,18 @@
 // Libraries
-import React from 'react';
-import { IoCloseSharp } from 'react-icons/io5';
-import { FaSearch } from 'react-icons/fa';
+import React from "react";
+import { IoCloseSharp } from "react-icons/io5";
+import { FaSearch } from "react-icons/fa";
 
 //Components
-import SearchItem from '../../Components/SearchItem/SearchItem';
+import SearchItem from "../../Components/SearchItem/SearchItem";
 
 // Stylesheets
-import './SearchResult.css';
+import "./SearchResult.css";
 
 export default function SearchResult({ result, onSearch, onSelect }) {
   const resultComponents =
     result && result.length ? (
-      result.map(item => (
+      result.map((item) => (
         <SearchItem
           bookId={item._id}
           author={item.author}
@@ -39,51 +39,7 @@ export default function SearchResult({ result, onSearch, onSelect }) {
         />
         <hr />
       </div>
-      {/* <div>{resultComponents}</div> */}
-      <div className="search-result__results">
-        <SearchItem
-          author="نویسنده"
-          cover={require('../../images/cover.png')}
-          name="اسم کتاب"
-          stars="1"
-        />
-        <SearchItem
-          author="نویسنده"
-          cover={require('../../images/cover.png')}
-          name="اسم کتاب 2"
-          stars="2"
-        />
-        <SearchItem
-          author="نویسنده"
-          cover={require('../../images/cover.png')}
-          name="اسم کتاب 3"
-          stars="3"
-        />
-        <SearchItem
-          author="نویسنده"
-          cover={require('../../images/cover.png')}
-          name="اسم کتاب 4"
-          stars="4"
-        />
-        <SearchItem
-          author="نویسنده"
-          cover={require('../../images/cover.png')}
-          name="اسم کتاب 5"
-          stars="5"
-        />
-        <SearchItem
-          author="نویسنده"
-          cover={require('../../images/cover.png')}
-          name="اسم کتاب 6"
-          stars="2"
-        />
-        <SearchItem
-          author="نویسنده"
-          cover={require('../../images/cover.png')}
-          name="اسم کتاب 7"
-          stars="4"
-        />
-      </div>
+      <div className="search-result__results">{resultComponents}</div>
     </div>
   );
 }
