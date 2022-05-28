05a1a20e53e62b16cc9082442ac2dabc4d5b17d8
SaraM2000 <saramovahhed@yahoo.com>
2022-05-28 19:22:50 +0430

    minor changes

--- src/Pages/AddChapter/AddChapter.js
+++ src/Pages/AddChapter/AddChapter.js
@@ -4,23 +4,28 @@ import React from "react";
 // Components
 import ChapterButton from "../../Components/AddChapter/ChapterButton/ChapterButton";
 import Button from "../../Components/UI/Button/Button";
-
+import ModalCard from "../../Components/UI/ModalCard/ModalCard";
 // Stylesheets
 import "./AddChapter.css";
 
 export default function AddChapter() {
   return (
-    <div className="add-chapter">
-      <div className="g-scrollbar add-chapter__chapter-container">
-        <ChapterButton />
-        <ChapterButton />
-        <ChapterButton />
-        <ChapterButton />
-        <ChapterButton />
+    <ModalCard show fill>
+      <div className="add-chapter">
+        <div className="g-scrollbar add-chapter__chapter-container">
+          <ChapterButton />
+          <ChapterButton />
+          <ChapterButton />
+          <ChapterButton />
+          <ChapterButton />
+          <ChapterButton />
+          <ChapterButton />
+          <ChapterButton />
+        </div>
+        <div className="add-chapter__controls">
+          <Button className="add-chapter__confirm-button">تایید</Button>
+        </div>
       </div>
-      <div className="add-chapter__controls">
-        <Button className="add-chapter__confirm-button">تایید</Button>
-      </div>
-    </div>
+    </ModalCard>
   );
 }
